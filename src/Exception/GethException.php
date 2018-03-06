a07a739ace6e9570b54c4df053d437da91d7e016
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-03-06 17:56:27 +0500

    GethException

--- src/Exception/GethException.php
+++ src/Exception/GethException.php
@@ -20,4 +20,16 @@ namespace EthereumRPC\Exception;
  */
 class GethException extends EthereumRPCException
 {
+    /**
+     * @param string $method
+     * @param string $expected
+     * @param string $got
+     * @return GethException
+     */
+    public static function unexpectedResultType(string $method, string $expected, string $got): self
+    {
+        return new self(
+            sprintf('Method [%s] expects result type %s, got %s', $method, strtoupper($expected), strtoupper($got))
+        );
+    }
 }
