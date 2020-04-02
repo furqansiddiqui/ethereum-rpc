cff5574a4f094a73ebd44f7ccf4f435335391c5d
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-04-02 20:12:20 +0500

    copyright update

--- src/Contracts/ABI/SHA3/Keccak.php
+++ src/Contracts/ABI/SHA3/Keccak.php
@@ -3,7 +3,7 @@
  * This file is a part of "furqansiddiqui/ethereum-rpc" package.
  * https://github.com/furqansiddiqui/ethereum-rpc
  *
- * Copyright (c) 2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ * Copyright (c) 2020 Furqan A. Siddiqui <hello@furqansiddiqui.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code or visit following link:
@@ -13,30 +13,6 @@
 /**
  * This file has been included for SHA3 compatibility from https://github.com/kornrunner/php-keccak
  * LICENSE of original author (Boris Momčilović) is included as below:
- */
-
-/**
- * MIT License
- *
- * Copyright (c) 2018 Boris Momčilović
-
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
-
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
-
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
  */
 
 declare(strict_types=1);
